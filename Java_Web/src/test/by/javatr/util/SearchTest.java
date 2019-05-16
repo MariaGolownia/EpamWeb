fd8c9c6e55bd3440ff7d734ec8f508c292a55d4c
Maria <mariagolownia@yandex.ru>
2019-05-16 21:56:21 +0300

    last commit

--- Java_Web/src/test/by/javatr/util/SearchTest.java
+++ Java_Web/src/test/by/javatr/util/SearchTest.java
@@ -6,10 +6,8 @@ import by.javatr.entity.TransportPlane;
 import by.javatr.util.Search;
 import by.javatr.util.TagForSearch;
 import org.testng.annotations.Test;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.testng.Assert.*;
 
 public class SearchTest {
